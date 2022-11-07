7bbf9548aee3c474d2fd569cde8264fe87bbeccc
JAG <jagrasso42@gmail.com>
2022-11-07 12:04:52 -0500

    Added endpoints for travellerRoutes

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -2,4 +2,15 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Location, Traveller, Trips } = require('../../models');
 
+// GET all travellers
+router.get('/', async (req, res) => {});
+
+// GET a single traveller
+router.get('/:id', async (req, res) => {});
+
+// Create a new traveller
+router.post('/', (req, res) => {});
+
+// DELETE a traveller by ID
+router.delete('/:id', async (req, res) => {});
 module.exports = router;
