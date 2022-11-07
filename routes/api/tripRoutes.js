0b097d18f488f77ab3df73fac3f668e5e5644f8e
JAG <jagrasso42@gmail.com>
2022-11-07 12:09:59 -0500

    added route for trips

---
+++ routes/api/tripRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Location, Traveller, Trips } = require('../../models');
+
+module.exports = router;
